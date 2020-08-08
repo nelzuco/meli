4320a67cdb07fa0f96d98046e98ba144a1753f75
unknown <nelsonzucolillo@gmail.com>
2020-08-07 23:08:35 -0300

    nuevo path

--- Documents/Java/ip-lookup/src/main/java/com/meli/iplookup/repository/CountryInfoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.meli.iplookup.repository;
-
-import com.meli.iplookup.model.CountryInfo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface CountryInfoRepository extends JpaRepository<CountryInfo, String> {
-}
